fea48007d4b84ce444c4231911767b1bbb14316f
Kamenow <iavorkamenov777@gmail.com>
2025-04-27 15:01:15 +0300

    Enhance ReviewPage functionality and UI
    - Refactored the `ReviewPageViewModel` to include flashcard review logic, such as updating the next review date based on user responses. Improved the UI in `ReviewPage.xaml` with better styling and layout for flashcards and buttons. Added swipe functionality for flipping cards and incorporated animations for a smoother user experience. Updated the code-behind in `ReviewPage.xaml.cs` to handle swipe events and initialize the ViewModel with the necessary services.

--- PocketDictionary/ViewModels/ReviewPageViewModel.cs
+++ PocketDictionary/ViewModels/ReviewPageViewModel.cs
@@ -3,13 +3,16 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using CommunityToolkit.Mvvm.Input;
-using PocketDictionary.Models; // Assuming your Flashcard model is there
+using Microsoft.Maui.Controls;
+using PocketDictionary.Models;
+using PocketDictionary.Services;
 
 namespace PocketDictionary.ViewModels
 {
     public class ReviewPageViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+        private readonly IFlashcardService _flashcardService;
 
         private void OnPropertyChanged([CallerMemberName] string propertyName = "")
             => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
@@ -24,10 +27,11 @@ namespace PocketDictionary.ViewModels
         public ICommand KnowCommand { get; }
         public ICommand DontKnowCommand { get; }
 
-        public ReviewPageViewModel(Deck deck)
+        public ReviewPageViewModel(Deck deck, IFlashcardService flashcardService)
         {
             Deck = deck;
-            FlashcardsToReview = new ObservableCollection<Flashcard>(deck.Flashcards);
+            _flashcardService = flashcardService;
+            FlashcardsToReview = new ObservableCollection<Flashcard>(deck.Flashcards.Where(f => f.NextReviewDate <= DateTime.Now));
 
             FlipCardCommand = new RelayCommand(FlipCard);
             KnowCommand = new RelayCommand(KnowCard);
@@ -50,7 +54,7 @@ namespace PocketDictionary.ViewModels
 
         public string CurrentFlashcardText => _isFrontVisible ? CurrentFlashcard?.Front : CurrentFlashcard?.Back;
 
-        public string RemainingFlashcardsText => $"{FlashcardsToReview.Count - _currentIndex} left";
+        public string RemainingFlashcardsText => $"Remaining cards: {FlashcardsToReview.Count - _currentIndex}";
 
         private void UpdateCurrentFlashcard()
         {
@@ -58,11 +62,13 @@ namespace PocketDictionary.ViewModels
             {
                 CurrentFlashcard = FlashcardsToReview[_currentIndex];
                 _isFrontVisible = true;
+                OnPropertyChanged(nameof(CurrentFlashcardText));
                 OnPropertyChanged(nameof(RemainingFlashcardsText));
             }
             else
             {
-                // TODO: Handle end of review
+                // End of review - navigate back
+                Application.Current.MainPage.Navigation.PopAsync();
             }
         }
 
@@ -74,12 +80,37 @@ namespace PocketDictionary.ViewModels
 
         private void KnowCard()
         {
+            if (CurrentFlashcard != null)
+            {
+                // Increment consecutive correct answers
+                CurrentFlashcard.CorrectAnswersInARow++;
+                
+                // Set next review date to current date + consecutive days
+                CurrentFlashcard.NextReviewDate = DateTime.Now.AddDays(CurrentFlashcard.CorrectAnswersInARow);
+                
+                // Save changes to database
+                _flashcardService.UpdateFlashcard(CurrentFlashcard);
+            }
+
             _currentIndex++;
             UpdateCurrentFlashcard();
         }
 
         private void DontKnowCard()
         {
+            if (CurrentFlashcard != null)
+            {
+                // Decrement consecutive correct answers (minimum 0)
+                CurrentFlashcard.CorrectAnswersInARow = Math.Max(0, CurrentFlashcard.CorrectAnswersInARow - 1);
+                
+                // Set next review date to current date + 2 * consecutive days
+                int daysToAdd = 2 * Math.Max(1, CurrentFlashcard.CorrectAnswersInARow);
+                CurrentFlashcard.NextReviewDate = DateTime.Now.AddDays(daysToAdd);
+                
+                // Save changes to database
+                _flashcardService.UpdateFlashcard(CurrentFlashcard);
+            }
+
             _currentIndex++;
             UpdateCurrentFlashcard();
         }
--- PocketDictionary/Views/ReviewPage.xaml.cs
+++ PocketDictionary/Views/ReviewPage.xaml.cs
@@ -3,14 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PocketDictionary.Models;
+using PocketDictionary.ViewModels;
+using PocketDictionary.Services;
 
 namespace PocketDictionary.Views
 {
     public partial class ReviewPage : ContentPage
     {
-        public ReviewPage()
+        private readonly ReviewPageViewModel _viewModel;
+
+        public ReviewPage(Deck deck, IFlashcardService flashcardService)
         {
             InitializeComponent();
+            _viewModel = new ReviewPageViewModel(deck, flashcardService);
+            BindingContext = _viewModel;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            // Make sure swipe gestures work properly
+            CardSwipeView.SwipeStarted += OnSwipeStarted;
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            CardSwipeView.SwipeStarted -= OnSwipeStarted;
+        }
+
+        private void OnSwipeStarted(object sender, SwipeStartedEventArgs e)
+        {
+            // Optional: You can add visual feedback when swipe starts
+        }
+
+        public async void OnFlipInvoked(object sender, EventArgs e)
+        {
+            // Perform flip animation
+            await FlipCardAnimation();
+        }
+
+        private async Task FlipCardAnimation()
+        {
+            // First half of the animation - scale down horizontally
+            await CardFrame.ScaleXTo(0, 100, Easing.CubicIn);
+            
+            // Execute the command - this changes the text on the card
+            _viewModel.FlipCardCommand.Execute(null);
+            
+            // Second half of the animation - scale back up
+            await CardFrame.ScaleXTo(1, 100, Easing.CubicOut);
         }
     }
 }
